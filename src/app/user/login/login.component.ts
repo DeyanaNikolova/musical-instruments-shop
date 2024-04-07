bb34699c4fd6d8d4c2a38eb372282bcf7d2f2808
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-04-07 17:27:40 +0100

    Build login form

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,14 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../user.service';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent  implements OnInit {
+export class LoginComponent implements OnInit {
+  returnUrl: string = '';
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private userService: UserService,
+    private router: Router
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'];
+  }
 
+  login() {
+    if(this.loginForm.invalid){
+      return;
+    }
+   
+  }
+  get fc() {
+    return this.loginForm.controls;
+  }
 }
