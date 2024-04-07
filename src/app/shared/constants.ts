4f62f73479ac20fb399439ab66b6ad6d23e01588
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-04-07 17:25:05 +0100

    Create configs.ts

---
+++ src/app/shared/constants.ts
@@ -0,0 +1,9 @@
+export const BASE_URL = 'http://localhost:3030';
+
+
+export const LOGIN_URL = BASE_URL + '/users/login';
+export const REGISTER_URL = BASE_URL + '/users/register';
+export const LOGOUT_URL = BASE_URL + '/users/logout';
+
+
+
