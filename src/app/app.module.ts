079db06bac79e3f654a90c6405b34a64287208d9
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-04-07 14:21:21 +0100

    Import ion icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    IonicModule
   ],
   providers: [
     provideClientHydration()
